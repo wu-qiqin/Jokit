9f098caf630e965273f2c0cb257e0db2a108a2c3
joe <i-guitar@outlook.com>
2020-04-26 11:05:51 +0800

    添加‘判断字符串是否经过url encode’方法

--- src/main/java/com/hujinwen/utils/EncryptUtils.java
+++ src/main/java/com/hujinwen/utils/EncryptUtils.java
@@ -5,6 +5,7 @@ import org.apache.commons.lang3.StringUtils;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
+import java.util.BitSet;
 
 /**
  * Created by joe on 2020/4/9
@@ -21,6 +22,34 @@ public class EncryptUtils {
                     -1, -1, -1, 62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
                     19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1,
                     -1, -1};
+
+    private static BitSet dontNeedEncoding;
+
+    static {
+        dontNeedEncoding = new BitSet(256);
+        int i;
+        for (i = 'a'; i <= 'z'; i++) {
+            dontNeedEncoding.set(i);
+        }
+        for (i = 'A'; i <= 'Z'; i++) {
+            dontNeedEncoding.set(i);
+        }
+        for (i = '0'; i <= '9'; i++) {
+            dontNeedEncoding.set(i);
+        }
+        dontNeedEncoding.set('+');
+        /**
+         * 这里会有误差,比如输入一个字符串 123+456,它到底是原文就是123+456还是123 456做了urlEncode后的内容呢？<br>
+         * 其实问题是一样的，比如遇到123%2B456,它到底是原文即使如此，还是123+456 urlEncode后的呢？ <br>
+         * 在这里，我认为只要符合urlEncode规范的，就当作已经urlEncode过了<br>
+         * 毕竟这个方法的初衷就是判断string是否urlEncode过<br>
+         */
+        dontNeedEncoding.set('-');
+        dontNeedEncoding.set('_');
+        dontNeedEncoding.set('.');
+        dontNeedEncoding.set('*');
+        dontNeedEncoding.set('~');
+    }
 
     /**
      * 加密
@@ -194,6 +223,50 @@ public class EncryptUtils {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /**
+     * 判断str是否urlEncoder.encode
+     * <p>
+     * 支持JAVA的URLEncoder.encode出来的string做判断。 即: 将' '转成'+'
+     * 0-9a-zA-Z保留
+     * '-'，'_'，'.'，'*'保留
+     * 其他字符转成%XX的格式，X是16进制的大写字符，范围是[0-9A-F]
+     *
+     * @param str 需判断的字符串
+     * @return 结果
+     */
+    public static boolean isUrlEncoded(String str) {
+        boolean needEncode = false;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (dontNeedEncoding.get((int) c)) {
+                continue;
+            }
+            if (c == '%' && (i + 2) < str.length()) {
+                // 判断是否符合urlEncode规范
+                char c1 = str.charAt(++i);
+                char c2 = str.charAt(++i);
+                if (isDigit16Char(c1) && isDigit16Char(c2)) {
+                    continue;
+                }
+            }
+            // 其他字符，肯定需要urlEncode
+            needEncode = true;
+            break;
+        }
+
+        return !needEncode;
+    }
+
+    /**
+     * 判断c是否是16进制的字符
+     *
+     * @param c 需判断的字符
+     * @return boolean
+     */
+    public static boolean isDigit16Char(char c) {
+        return (c >= '0' && c <= '9') || (c >= 'A' && c <= 'F');
     }
 
 }
